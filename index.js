3a891a44d93d95e5b444595211fc034e3eb7349d
Brian Dunnette <dunn0172@umn.edu>
2016-07-13 15:29:50 -0500

    adding preliminary grammar & tweet functions

--- index.js
+++ index.js
@@ -3,7 +3,26 @@ var tracery = require('tracery-grammar');
 
 var twitterCredentials = require('./twitter_credentials.json');
 var twitter = new Twit(twitterCredentials);
+
 var grammarData = require('./grammar.json');
 var grammar = tracery.createGrammar(grammarData);
 grammar.addModifiers(tracery.baseEngModifiers);
-console.log(grammar.flatten('#origin#'));
+
+function generateTweet() {
+    var tweet = grammar.flatten('#sentence#');
+    return tweet;
+}
+
+function sendTweet() {
+    var tweet = generateTweet();
+    console.log("Sending tweet: " + tweet);
+    twitter.post('statuses/update', {
+        status: tweet
+    }, function(err, data, response) {
+        console.log(data)
+    })
+}
+
+var timer = setInterval(function() {
+    sendTweet()
+}, 300000);
