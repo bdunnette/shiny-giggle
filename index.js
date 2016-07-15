99999edc152cdaa4668dad492e16f27328207c0d
Brian Dunnette <dunn0172@umn.edu>
2016-07-15 09:54:25 -0500

    yay markov chains!

--- index.js
+++ index.js
@@ -1,30 +1,40 @@
 var Twit = require('twit');
-var tracery = require('tracery-grammar');
+var Markov = require('markov-strings');
 
 var twitterCredentials = require('./twitter_credentials.json');
 var twitter = new Twit(twitterCredentials);
 
-var grammarData = require('./grammar.json');
-var grammar = tracery.createGrammar(grammarData);
-grammar.addModifiers(tracery.baseEngModifiers);
+var data = require('./tweets.json');
+// Some options to generate Twitter-ready strings
+const options = {
+    maxLength: 140,
+    minWords: 5,
+    stateSize: 1,
+    minScore: 3
+};
+
+// Instantiate the generator
+const markov = new Markov(data, options);
 
 function generateTweet() {
-    var tweet = grammar.flatten('#sentence#');
+    var tweet = markov.generateSentenceSync();
     return tweet;
 }
 
 function sendTweet() {
-    var tweet = generateTweet();
+    var tweet = generateTweet().string;
     console.log("Sending tweet: " + tweet);
     twitter.post('statuses/update', {
         status: tweet
     }, function(err, data, response) {
-        console.log(data)
+        // console.log(data)
     })
 }
 
-sendTweet();
-
-var timer = setInterval(function() {
-    sendTweet()
-}, 600000);
+markov.buildCorpus()
+    .then(() => {
+        sendTweet();
+        var timer = setInterval(function() {
+            sendTweet()
+        }, 600000);
+    })
